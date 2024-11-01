3f30751ecb397bdd57604971110f73a5d46c8bc0
Isaiah Strouse <113122370+StrizzyAllen@users.noreply.github.com>
2024-11-01 09:27:33 -0500

    intitial commit

---
+++ addToSharedMailbox.ps1
@@ -0,0 +1,24 @@
+﻿# Connect to exchange online first - make sure you have the correct permissions or use the IT admin credential
+
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline 
+
+# Define the user (user email) and the shared mailboxes (mailbox address) you want to add them too
+
+$userEmail = "first.last@companydomain.com"
+$sharedMailboxes = @("department@companiydomain.com")
+
+# loop through each mailbox and add full access
+foreach ($mailbox in $sharedMailboxes) {
+    # Grant full access 
+    Add-MailboxPermission -identity $mailbox -user $userEmail -AccessRights fullAccess -inheritanceType All -autoMapping $false
+    
+    # Grant Send As Permission
+    Add-RecipientPermission -Identity $mailbox -Trustee $userEmail -AccessRights SendAs
+}
+
+Write-Host "Send as permissions have been granted to $userId for $sharedMailboxes"
+
+# Disconnect from the exchange server
+
+Disconnect-ExchangeOnline
---
+++ userToGroup.ps1
@@ -0,0 +1,16 @@
+﻿Connect-MgGraph
+
+$userId = "be27d9dd-bb37-4242-9d3c-42f3d56ec74b"
+$groupIds = @("bc17c961-b025-4bb3-bdda-2b5e87178838", "fc783e10-3f6b-4970-8d7a-5e55f21b2bce", "b58b1d4b-2d65-4424-adbc-89150dd3cec9", "5d36de56-e8eb-401f-a394-f018d6c4ebe0","3c8c5960-af02-4a3c-a4f4-9da174cff01e", "76f70fca-b63f-4664-8806-1d354af6a9a3")
+
+foreach ($groupId in $groupIds) {
+    try {
+        New-MgGroupMember -GroupId $groupId -DirectoryObjectId $userId
+        Write-Host "Added $userId to $groupId"
+   } catch {
+        Write-Host "Failed to add user"
+    }
+}
+
+
+Disconnect-MgGraph
